17ff96fa108a98384084dacc64815c094ffa95a7
perlancar (@pc-office) <perlancar@gmail.com>
2016-06-16 12:04:55 +0700

    Add sample data from URI-Escape-XS distribution

--- lib/Bencher/Scenario/URIEscaping.pm
+++ lib/Bencher/Scenario/URIEscaping.pm
@@ -36,23 +36,41 @@ our $scenario = {
             include_participant_tags => ['escape'],
             args => {str=>''},
         },
+        # sample data from URI-XSEscape distribution
         {
             name => 'ascii53',
             tags => ['escape'],
             include_participant_tags => ['escape'],
             args => {str=>'I said this: you / them ~ us & me _will_ "do-it" NOW!'},
         },
+        # sample data from URI-XSEscape distribution
         {
             name => 'utf36',
             tags => ['escape', 'utf8'],
             include_participant_tags => ['escape & utf8'],
             args => {str=>'http://www.google.co.jp/search?q=小飼弾'},
         },
+        # sample data from URI-XSEscape distribution
         {
             name => 'u_ascii53',
             tags => ['unescape'],
             include_participant_tags => ['unescape'],
             args => {str=>'I%20said%20this%3a%20you%20%2f%20them%20~%20us%20%26%20me%20_will_%20%22do-it%22%20NOW%21'},
+        },
+
+        # sample data from URI-Escape-XS distribution
+        {
+            name => 'ascii66',
+            tags => ['escape'],
+            include_participant_tags => ['escape'],
+            args => {str=>'https://stackoverflow.com/questions/3629212/how can perls xsub die'},
+        },
+        # sample data from URI-Escape-XS distribution
+        {
+            name => 'u_ascii66',
+            tags => ['unescape'],
+            include_participant_tags => ['unescape'],
+            args => {str=>'https%3A%2F%2Fstackoverflow.com%2Fquestions%2F3629212%2Fhow%20can%20perls%20xsub%20die'},
         },
     ],
 };
